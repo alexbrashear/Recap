06ad82e59f127867c3925bdb9a4fa45d0a4e2b5a
Alex Brashear <alexjbrashear@gmail.com>
2017-01-28 15:04:23 -0500

    access camera in full screen

--- MeMailer5000/CameraViewController.swift
+++ MeMailer5000/CameraViewController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class CameraViewController: UIViewController {
+    
+    @IBOutlet private var captureView: UIView!
+    
+    private var takePhoto = UIButton()
+    
+    var session = AVCaptureSession()
+    var stillImageOutput: AVCapturePhotoOutput?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        session.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        let backCamera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        var input: AVCaptureDeviceInput!
+        do {
+            input = try AVCaptureDeviceInput(device: backCamera)
+        } catch let error as NSError {
+            assertionFailure(error.localizedDescription)
+        }
+        
+        if session.canAddInput(input) {
+            session.addInput(input)
+            // ...
+            // The remainder of the session setup will go here...
+            stillImageOutput = AVCapturePhotoOutput()
+//            stillImageOutput?.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+        }
+        
+        if session.canAddOutput(stillImageOutput) {
+            session.addOutput(stillImageOutput)
+            // ...
+            // Configure the Live Preview here...
+            
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
+            videoPreviewLayer!.videoGravity = AVLayerVideoGravityResizeAspectFill
+            videoPreviewLayer!.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+            captureView.layer.addSublayer(videoPreviewLayer!)
+            session.startRunning()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        videoPreviewLayer!.frame = captureView.frame
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
