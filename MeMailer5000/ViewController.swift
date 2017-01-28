5dc1fa90b43ea6d6490e53e244f16270ad90f9a9
Alex Brashear <alexjbrashear@gmail.com>
2017-01-28 13:11:04 -0500

    Initial Commit

---
+++ MeMailer5000/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeMailer5000
+//
+//  Created by Alex Brashear on 1/28/17.
+//  Copyright © 2017 memailer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
