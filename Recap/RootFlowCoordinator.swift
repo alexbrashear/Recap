745b28313a2b29f906abee1d38e148765abf13c4
Alex Brashear <alexjbrashear@gmail.com>
2017-05-31 23:50:33 -0400

    refactor: rename flows

--- Recap/RootFlowCoordinator.swift
+++ Recap/RootFlowCoordinator.swift
@@ -43,58 +43,6 @@ class RootFlowCoordinator {
         }
     }
     
-    /// Configures a `CameraViewController`
-    ///
-    /// - Parameter vc: the view controller to configure
-    func configure(vc: CameraViewController, nc: UINavigationController) {
-        let sendPhoto: SendPhoto = { [weak self, weak vc] image in
-            guard self?.filmController.canTakePhoto() ?? false else {
-                vc?.presentAlert(.errorSending(PhotoError.noFilmError))
-                return
-            }
-            guard let address = self?.userController.user?.address else { return }
-            vc?.presentAlert(.uploadingRecap)
-            self?.postcardSender.send(image: image, to: address) { result in
-                let (photo, error) = result
-                DispatchQueue.main.async {
-                    switch (photo, error) {
-                    case let (_, .some(error)):
-                        vc?.returnToCamera(with: .errorSending(error))
-                    case (.none, .none):
-                        vc?.returnToCamera(with: .errorSending(PhotoError.unknownFailure))
-                    case let (.some(photo), .none):
-                        let remainingPhotos = self?.filmController.useFilmSlot(photo) ?? 0
-                        vc?.returnToCamera(with: .successfulSend)
-                        vc?.overlay.updateCount(to: remainingPhotos)
-                    }
-                }
-            }
-        }
-        let sentPostcardsTapHandler: SentPostcardsTapHandler = { [weak self, weak nc] _ in
-            guard let nc = nc else { return }
-            self?.pushSentPhotosController(onto: nc)
-        }
-        
-        let showSettings: () -> Void = { [weak self, weak vc] in
-            guard let vc = vc else  { return }
-            self?.presentSettingsViewController(from: vc)
-        }
-        
-        let countAction: CountAction = { [weak self, weak vc] in
-            guard let vc = vc else { return }
-            self?.presentPurchaseController(from: vc)
-        }
-        
-        let vm = CameraViewModel(initialCount: filmController.currentFilm?.remainingPhotos ?? 0, sendPhoto: sendPhoto, sentPostcardsTapHandler: sentPostcardsTapHandler, showSettings: showSettings, countAction: countAction)
-        vc.viewModel = vm
-    }
-    
-    func pushCameraViewController(onto nc: UINavigationController) {
-        guard let cameraViewController = R.storyboard.camera.cameraViewController() else { fatalError() }
-        configure(vc: cameraViewController, nc: nc)
-        nc.pushViewController(cameraViewController, animated: true)
-    }
-    
     func configureNavigationController(nc: UINavigationController) {
         nc.navigationBar.barTintColor = .rcpAzure
         nc.navigationBar.tintColor = .white
