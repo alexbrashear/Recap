863d61a452fad24fec7d989776d340c73e94bfab
Alex Brashear <alexjbrashear@gmail.com>
2017-06-03 13:33:15 -0400

    feat: add custom back image

--- Recap/PostcardProvider.swift
+++ Recap/PostcardProvider.swift
@@ -25,22 +25,9 @@ class PostcardProvider {
         }
     }
     
-    func persist(postcard: Postcard) {
-        let connection = DatabaseController.sharedInstance.newWritingConnection()
-        connection.readWrite { transaction in
-            var postcards = transaction.object(forKey: "postcards", inCollection: "postcards") as? [Postcard] ?? []
-            if postcards.isEmpty {
-                transaction.setObject([postcard], forKey: "postcards", inCollection: "postcards")
-            } else {
-                postcards.append(postcard)
-                transaction.replace(postcards, forKey: "postcards", inCollection: "postcards")
-            }
-        }
-    }
-    
     private func metadata(forAddress address: Address, imageUrlString: String) -> Data? {
         let front = "<html><head><meta charset=\"UTF-8\"><link href=\"https://fonts.googleapis.com/css?family=Open+Sans\" rel=\"stylesheet\" type=\"text/css\"><title>Lob.com Sample 4x6 Postcard Front</title><style>*, *:before, *:after {-webkit-box-sizing: border-box;-moz-box-sizing: border-box;    box-sizing: border-box;  }  body {    width: 6.25in;    height: 4.25in;    margin: 0;    padding: 0;    /* If using an image, the background image should have dimensions of 1875x1275 pixels. */    background-image: url(\"\(imageUrlString)\");    background-size: cover;    background-repeat: no-repeat; background-position: center;  }  #safe-area {    position: center;    width: 5.875in;    height: 3.875in;    left: 0.1875in;    top: 0.1875in;    background-color: rgba(255,255,255,0.5);  }  .text {    margin: 10px;    font-family: \"Open Sans\";    font-weight: 400    font-size: 40px;    color: white;    text-shadow: 2px 2px black;  }</style></head><body>  </body></html>"
-        let message = "this is a test from Alex and Dave"
-        return "\(address.bodyString(withParent: "to"))&message=\(message)&front=\(front)".data(using: .utf8)
+        let back = "https://s3.amazonaws.com/recap-static-images/recap_watermark.png"
+        return "\(address.bodyString(withParent: "to"))&back=\(back)&front=\(front)".data(using: .utf8)
     }
 }
