984c100b280e0ceac681ad53d5d8971e5141bd57
Alex Brashear <alexjbrashear@gmail.com>
2017-07-30 11:14:16 -0400

    feat: give user two recaps when signing up

--- Recap/UserController.swift
+++ Recap/UserController.swift
@@ -70,7 +70,8 @@ class UserController {
         ///
         
         let createAddressInput = CreateAddressInput(city: address.city, secondaryLine: address.line2, name: address.name, primaryLine: address.line1, zipCode: address.zip, state: address.state)
-        let createUserInput = CreateUserInput(remainingPhotos: 2, username: email, address: createAddressInput, password: password)
+        let createFilmInput = CreateFilmInput(capacity: 2)
+        let createUserInput = CreateUserInput(username: email, address: createAddressInput, film: createFilmInput, password: password)
         let mut = SignupUserMutation(user: createUserInput)
         
         graphql.client.perform(mutation: mut, queue: .main) { [weak self] result, error in
