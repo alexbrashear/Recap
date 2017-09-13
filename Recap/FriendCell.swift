4ea5802df0e3a837896c769f4176a6e0170063fd
Alex Brashear <alexjbrashear@gmail.com>
2017-09-12 23:05:44 -0400

    wip: sample friends list controller

---
+++ Recap/FriendCell.swift
@@ -0,0 +1,19 @@
+//
+//  FriendCell.swift
+//  Recap
+//
+//  Created by Alex Brashear on 9/11/17.
+//  Copyright © 2017 memailer. All rights reserved.
+//
+
+import UIKit
+import Reusable
+
+class FriendCell: UITableViewCell, NibReusable {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Recap/FriendsListController.swift
@@ -0,0 +1,64 @@
+//
+//  FriendsListController.swift
+//  Recap
+//
+//  Created by Alex Brashear on 9/11/17.
+//  Copyright © 2017 memailer. All rights reserved.
+//
+
+import UIKit
+
+class FriendsListController: UIViewController {
+    
+    var tableView: UITableView!
+    var bottomBar = UIView(frame: .zero)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        bottomBar.backgroundColor = .red
+        tableView = UITableView(frame: .zero)
+        tableView.backgroundColor = .white
+        
+        view.addSubview(tableView)
+        view.addSubview(bottomBar)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        bottomBar.translatesAutoresizingMaskIntoConstraints = false
+        bottomBar.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        bottomBar.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        bottomBar.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        tableView.bottomAnchor.constraint(equalTo: bottomBar.topAnchor).isActive = true
+        
+        tableView.register(cellType: FriendCell.self)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension FriendsListController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(for: indexPath) as FriendCell
+        return cell
+    }
+}
+
+extension FriendsListController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
